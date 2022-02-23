70d20a4750bc9b4e4ef552406b080047f5a57309
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-23 10:39:57 -0800

    update

---
+++ test/learning.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // // sum of the value from 1 to 10
+    // int sum = 0, val = 1;
+    // while (val <= 10) {
+    //     sum += val;
+    //     val++;
+    // }
+    // return sum;
+    // //rewrite using for loop
+    // int sum = 0;
+    // for (int val = 1; val <= 10; val++) {
+    //     sum += val;
+    // }
+    // return sum;
+    // //exercise
+    // int sum = 0, val = 50;
+    // while (val <= 100) {
+    //     sum += val;
+    //     val++;
+    // }
+    // return sum;
+    // //Exercise 1.11
+    // cout << "Please enter two integers" << endl;
+    // int i, j;
+    // cin >> i >> j;
+    // for (int v = i; v <= j; v++) {
+    //     cout << v << endl;
+    // }
+    // rewrite using while loop;
+    // cout << "Please enter two integers" << endl;
+    // int i , j;
+    // cin >> i >> j;
+    // int val = i;
+    // while (val <= j) {
+    //     cout << val << endl;
+    // }
+    int sum = 0, val = 0;
+    while (cin >> val) {
+        sum += val;
+    }
+    cout << sum << endl;
+    return sum;
+}
